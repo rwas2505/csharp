c85080605f71e11189a8b56931c2d3c206ca23d8
Ryan <ryanwaszak@gmail.com>
2020-06-15 10:15:20 -0500

    add Formatting and NullHandling

---
+++ Chapter02/Formatting/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfApples = 12;
+            decimal pricePerApple = 0.35M;
+
+            Console.WriteLine(
+                format: "{0} apples costs {1:C}",
+                arg0: numberOfApples,
+                arg1: pricePerApple * numberOfApples
+            );
+            string formatted = string.Format(
+                format: "{0} apples costs {1:C}",
+                arg0: numberOfApples,
+                arg1: pricePerApple * numberOfApples
+            );
+
+            //WriteToFile(formatted); //writes the string into a file
+
+            Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
+
+            string applesText = "Apples";
+            int applesCount = 1234;
+            string bananasText = "Bananas";
+            int bananasCount = 56789;
+
+            Console.WriteLine(
+                format: "{0, -8} {1,6:N0}",
+                arg0: "Name",
+                arg1: "Count"
+            );
+            Console.WriteLine(
+                format: "{0, -8} {1,6:N0}",
+                arg0: applesText,
+                arg1: applesCount
+            );
+            Console.WriteLine(
+                format: "{0, -8} {1,6:N0}",
+                arg0: bananasText,
+                arg1: bananasCount
+            );
+
+            Console.Write("Type yoru first name and press ENTER: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Type your age and press ENTER: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine(
+                $"Hello {firstName}, you look good for {age}."
+            );
+        }
+    }
+}
--- Chapter02/NullHandling/Program.cs
+++ Chapter02/NullHandling/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+#nullable enable
 
 namespace NullHandling
 {
+    class Address
+    {
+        public string? Building; //notice the ? an the #nullable enable. Also see .csproj for the nullable element added
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -10,13 +18,19 @@ namespace NullHandling
             //thisCannotBeNull = null; //compile error!
 
             int? thisCouldBeNull = null;
-            Console.WriteLine(thisCouldBeNull); //first line of output is blank since it is null
+            Console.WriteLine(thisCouldBeNull); //first line of output is blank since it is null 
             Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
             
             thisCouldBeNull = 7;
 
             Console.WriteLine(thisCouldBeNull);
             Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = "London";
+            address.Region = null;
         }
     }
 }
