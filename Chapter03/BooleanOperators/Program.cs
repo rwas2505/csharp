781248a8cec16e59ec419b8174c5bd863ae638cc
Ryan <ryanwaszak@gmail.com>
2020-06-19 10:02:50 -0500

    add Conditional Logic Operators portion

--- Chapter03/BooleanOperators/Program.cs
+++ Chapter03/BooleanOperators/Program.cs
@@ -24,6 +24,15 @@ namespace BooleanOperators
             WriteLine($"a   |{a ^ a, -5} | {a ^ b, -5} ");
             WriteLine($"b   |{b ^ a, -5} | {b ^ b, -5} ");
             WriteLine();
+
+            WriteLine($"a && DoStuff() = {a && DoStuff()}");
+            WriteLine($"b && DoStuff() = {b && DoStuff()}"); //DoStuff function was not executed!
+        }
+
+        private static bool DoStuff()
+        {
+            WriteLine("I am doing some stuff.");
+            return true;
         }
     }
 }
