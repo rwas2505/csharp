d68409da8ede100666435b35152a007a44c38ae9
Ryan Waszak <rwaszak@tesla.com>
2020-06-23 19:07:35 -0700

    add HandlingExceptions

---
+++ Chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Before Parsing");
+
+            Write("What is your age? ");
+            string input = ReadLine();
+
+            try
+            {
+                int age = int.Parse(input);
+                WriteLine($"You are {age} years old.");
+            }
+            catch (OverflowException)
+            {
+                WriteLine("Your age is a valid number format but it is either too big or too small.");
+            }
+            catch (FormatException)
+            {
+                WriteLine("The age you entered is not a valid number format.");
+            }
+            catch(Exception ex)
+            {
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+            WriteLine("After parsing");
+        }
+    }
+}
