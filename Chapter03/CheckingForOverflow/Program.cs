91df32e2fedc931cf65a7237328e515d0ba84e46
Ryan Waszak <rwaszak@tesla.com>
2020-06-24 08:08:40 -0700

    add CheckingForOverflow/

---
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace CheckingForOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                checked
+                {
+                    int x = int.MaxValue - 1;
+                    WriteLine($"Initial value: {x}");
+                    x++;
+                    WriteLine($"After Incrementing: {x}");
+                    x++;
+                    WriteLine($"After Incrementing: {x}");
+                    x++;
+                    WriteLine($"After Incrementing: {x}");
+                }
+            }
+            catch (OverflowException)
+            {
+                WriteLine("The code overflowed but I caught the exception.");
+            }
+        }
+    }
+}
