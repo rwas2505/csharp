0aa367c7b14fadead5ce253a4e02df1dfe3aacfc
Ryan Waszak <rwaszak@tesla.com>
2020-06-25 16:10:15 -0700

    add Exercise 3.2 to Exercises/Program.cs

---
+++ Chapter03/Excercise02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace Excercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 255;
+            for (byte i = 0; i < max; i++)
+            if (max > 255)
+            {
+                WriteLine($"Infinite loop will occur unless max is 255 or less. Since max is {max}, loop has been cancelled");
+                break;
+            }
+            else
+            {
+                WriteLine(i);
+            }
+
+            // byte num = 255;
+            // num++;
+            // num++;
+            // WriteLine(num);
+        }
+    }
+}
