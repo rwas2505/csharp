9de369e1ccdb424842030b20142adcd68178226c
Ryan Waszak <rwaszak@tesla.com>
2020-06-25 16:26:48 -0700

    add Exercise03 (fizzbuzz)

---
+++ Chapter03/Exercise03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            for (int y = 1; y < 101; y++)
+            {
+                if (y % 3 == 0 && y % 5 == 0)
+                {
+                    WriteLine("FizzBuzz");
+                }
+                else if (y % 3 == 0)
+                {
+                    WriteLine("Fizz");
+                }
+                else if (y % 5 == 0)
+                {
+                    WriteLine("Buzz");
+                }
+                else 
+                {
+                    WriteLine(y);
+                }
+            }
+        }
+    }
+}
