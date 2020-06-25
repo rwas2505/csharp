4bcaecf88ea3971065931b4b79e0734eb3ee7f0a
Ryan Waszak <rwaszak@tesla.com>
2020-06-25 16:34:41 -0700

    add Exercise 3.4 partial

---
+++ Chapter03/Exercise04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a number between 0 and 255: ");
+            string y = ReadLine();
+            WriteLine(y);
+            
+
+        }
+    }
+}
