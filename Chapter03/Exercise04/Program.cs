af481cb2adb3ee8857ff749126196f5c497c4c32
Ryan Waszak <rwaszak@tesla.com>
2020-06-26 09:30:39 -0700

    add completed Exercise04 app from Exercise 3.4 problem

--- Chapter03/Exercise04/Program.cs
+++ Chapter03/Exercise04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static System.Console;
+using static System.Convert;
 
 namespace Exercise04
 {
@@ -8,9 +9,21 @@ namespace Exercise04
         static void Main(string[] args)
         {
             Write("Enter a number between 0 and 255: ");
-            string y = ReadLine();
-            WriteLine(y);
-            
+            string first = ReadLine();
+
+            Write("Enter another number between 0 and 255: ");
+            string second = ReadLine();
+
+            try
+            {
+            int firstNum = int.Parse(first);
+            int secondNum = int.Parse(second);
+            WriteLine($"{firstNum} divided by {secondNum} is {firstNum / secondNum}");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
 
         }
     }
