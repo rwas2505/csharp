c629bd50a2673690b35202b3f08d70e8c85f6235
Ryan Waszak <rwaszak@tesla.com>
2020-06-28 13:45:16 -0700

    add Debugging

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Debugging
+{
+    class Program
+    {
+        /// <summary>
+        /// Accepts two double numbers and returns the sum of the two inputs
+        /// </summary>
+        /// <param name="a">Type double number to add to b</param>
+        /// <param name="b">Type double number to add to a</param>
+        /// <returns>Sum of type double a and b</returns>
+        static double Add(double a, double b)
+        {
+            return a + b; //deliberate bug!
+        }
+        static void Main(string[] args)
+        {
+            double a = 4.5; //or use var
+            double b = 2.5;
+            double answer = Add(a,b);
+            WriteLine($"{a} + {b} = {answer}");
+            ReadLine(); //wait for user to press ENTER
+        }
+    }
+}
