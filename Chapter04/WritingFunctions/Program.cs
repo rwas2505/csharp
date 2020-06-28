9a58cc349b0de2870d52b372424d029360852351
Ryan Waszak <rwaszak@tesla.com>
2020-06-27 17:55:15 -0700

    add CardinalToOrdinal function

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -115,10 +115,51 @@ namespace WritingFunctions
             }
             while (isNumber);
         }
+
+        static string CardinalToOrdinal(int number)
+        {
+            switch (number)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return $"{number}th";
+                default:
+                    string numberAsText = number.ToString();
+                    char lastDigit = numberAsText[numberAsText.Length - 1];
+                    string suffix = string.Empty;
+                    switch (lastDigit)
+                    {
+                        case '1':
+                            suffix = "st";
+                            break;
+                        case '2':
+                            suffix = "nd";
+                            break;
+                        case '3':
+                            suffix = "rd";
+                            break;
+                        default:
+                            suffix = "th";
+                            break;
+                    }
+                    return $"{number}{suffix}";
+            }
+        }
+
+        static void RunCardinalToOridnal()
+        {
+            for (int number = 1; number <=40; number++)
+            {
+                Write($"{CardinalToOrdinal(number)}");
+                WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
             // RunTimesTable();
-            RunCalculateTax();
+            // RunCalculateTax();
+            RunCardinalToOridnal();
         }
     }
 }
