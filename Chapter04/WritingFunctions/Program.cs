1b94b18194b2e20e738b8a4a2346ac16a1b2bac4
Ryan Waszak <rwaszak@tesla.com>
2020-06-28 08:19:42 -0700

    add Factorial function

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -5,6 +5,45 @@ namespace WritingFunctions
 {
     class Program
     {
+
+        static int Factorial (int number)
+        {
+            if (number < 1)
+            {
+                return 0;
+            }
+            else if (number==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number-1);    
+            }
+        }
+
+        static void RunFactorial()
+        {
+            bool isNumber;
+            do
+            {
+                Write("Enter a number: ");
+
+                isNumber = int.TryParse(ReadLine(), out int number);
+
+                if (isNumber)
+                {
+                    WriteLine($"{number:N0}! = {Factorial(number):N0}");
+                }
+                else
+                {
+                    WriteLine("You did not enter a valid number!");
+                }
+            }
+            while(isNumber);
+        }
+
+
         //with switch expressions showing syntax found on SO for multiple cases on a given output
         static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
         {
@@ -159,7 +198,8 @@ namespace WritingFunctions
         {
             // RunTimesTable();
             // RunCalculateTax();
-            RunCardinalToOridnal();
+            // RunCardinalToOridnal();
+            RunFactorial();
         }
     }
 }
