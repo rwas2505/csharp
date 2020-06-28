232321cc25cba62f093ad20d46882d6869eb9775
Ryan Waszak <rwaszak@tesla.com>
2020-06-28 15:07:39 -0700

    add switching trace levels

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace Instrumenting
 {
@@ -13,9 +14,28 @@ namespace Instrumenting
 
             //text writer is buffered, so this option calls Flush() on all listeners after writing
             Trace.AutoFlush = true;
-            
+
             Debug.WriteLine("Debug says, I am watching!");
             Trace.WriteLine("Trace says, I am watching!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json",
+                    optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(
+                displayName: "PacktSwitch",
+                description: "This switch is set via a JSON config."
+            );
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
         }
     }
 }
