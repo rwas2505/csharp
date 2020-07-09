d4ff777684d89ebb77d91ee82067b63c7dc4fda3
Ryan Waszak <rwaszak@tesla.com>
2020-07-09 09:18:01 -0700

    add instance and static method to Person class for procreating and implement in PeopleApp

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 namespace Packt.Shared
 {
     public class Person
     {
+        //fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        //methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+        //static method to "multiply"
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+
+            return baby;
+        }
+
+        //instance method to "multiply"
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner); //this is a keyword that references the current instance of the class
+        }
     }
 }
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Packt.Shared;
+using static System.Console;
 
 namespace PeopleApp
 {
@@ -6,7 +8,21 @@ namespace PeopleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var harry = new Person {Name = "Harry"};
+            var mary = new Person {Name = "Mary"};
+            var jill = new Person {Name = "Jill"};
+
+            //call instance method
+            var baby1 = mary.ProcreateWith(harry);
+
+            //call static method
+            var baby2 = Person.Procreate(harry, jill);
+
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+
+            WriteLine($"{harry.Name}'s first child is namedd \"{harry.Children[0].Name}\".");
         }
     }
 }
